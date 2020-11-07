6b9a6ea6b6089f426acd3d708e8a6f5513d25058
manasvi <manasvivaidyula3@gmail.com>
2020-11-07 17:32:42 +0530

    minor changes

--- covid19/src/components/CountryPicker/CountryPicker.jsx
+++ covid19/src/components/CountryPicker/CountryPicker.jsx
@@ -25,7 +25,7 @@ const CountryPicker = ({handleCountryChange}) => {
     return (
         <FormControl className={styles.FormControl}>
             <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
-                <option value="">United States</option>
+                <option value="">Global</option>
                 {fetchedCountries.map((country,i) => <option key={i} value={country}> {country} </option>)}
             </NativeSelect>
         </FormControl>
